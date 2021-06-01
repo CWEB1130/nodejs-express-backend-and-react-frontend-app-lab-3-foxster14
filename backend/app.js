cdf3250bd83c374f22923dbaaef2c8780672e4a1
sarahfox <sarah.fox.h@gmail.com>
2021-06-01 14:10:32 -0500

    finished final steps through step 10

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var offersRouter = require('./routes/offers');
 
 var app = express();
-
+//Sarah Fox
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
